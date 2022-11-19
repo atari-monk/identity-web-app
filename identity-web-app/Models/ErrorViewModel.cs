a2ecf076dca4111ec7e0ac9e98e4a3e860356da7
krzm <kmazanek@gmail.com>
2022-11-19 14:09:21 +0100

    mistake in app name-delete

--- identity-web-app/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace identity_web_app.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
