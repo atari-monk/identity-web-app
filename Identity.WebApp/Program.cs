5088e713f0ad60966cde5656759beaf86e56d788
krzm <kmazanek@gmail.com>
2022-11-19 20:28:38 +0100

    Add identity UI generated

--- Identity.WebApp/Program.cs
+++ Identity.WebApp/Program.cs
@@ -1,6 +1,14 @@
 using Identity.WebApp.Config;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Identity.WebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 new IdentityConfig(builder).RegisterServices();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddControllersWithViews();
