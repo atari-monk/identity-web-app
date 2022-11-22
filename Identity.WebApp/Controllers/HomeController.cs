58322223fede57e8730dea4d57803d219da23867
krzm <kmazanek@gmail.com>
2022-11-22 13:09:52 +0100

    add authorize, fix bugs

--- Identity.WebApp/Controllers/HomeController.cs
+++ Identity.WebApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Identity.WebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Identity.WebApp.Controllers;
 
@@ -18,6 +19,7 @@ public class HomeController : Controller
         return View();
     }
 
+    [Authorize]
     public IActionResult Privacy()
     {
         return View();
